94fce43297ec82f7b4e604002e02a71550e2a050
세훈 <wer5136@naver.com>
2023-09-27 13:03:51 +0900

    first

---
+++ variable.js
@@ -0,0 +1,14 @@
+'use strict';
+
+{
+let name = 'sehoon';
+console.log(name);
+name = 'hello';
+console.log(name);
+}
+
+console.log(name);
+console.log(name);
+console.log(name);
+console.log(name);
+console.log(name);
